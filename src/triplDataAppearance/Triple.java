4f6ca6cec774e93b4f4e77867ecd3106bac313da
Andriy Yendrizhyewskiy <for.a.kvadrat@gmail.com>
2022-12-03 16:02:10 +0200

    fix Triple constructor

--- src/triplDataAppearance/Triple.java
+++ src/triplDataAppearance/Triple.java
@@ -1,42 +1,21 @@
 package triplDataAppearance;
 
 public class Triple extends Tripl{
-    /**
-     *
-     */
-    public Triple(int i) {
-        super();
+    public Triple(Object o) {
+        super(o);
     }
-
-    /**
-     * @return
-     */
     @Override
     public int hashCode() {
         return super.hashCode();
     }
-
-    /**
-     * @param obj
-     * @return
-     */
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
     }
-
-    /**
-     * @return
-     * @throws CloneNotSupportedException
-     */
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
-
-    /**
-     * @return
-     */
     @Override
     public String toString() {
         return super.toString();
